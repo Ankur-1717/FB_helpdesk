c4f7eb5b35594b6ef50d6efb58df9101bea08456
Ankur-1717 <ankur.ambar17@gmail.com>
2024-02-15 23:19:53 +0530

    completed all UI

---
+++ helpdesk/src/Utils/constants.js
@@ -0,0 +1,1 @@
+export const access_token = process.env.ACCESS_TOKEN;
--- server/index.js
+++ server/index.js
@@ -1,18 +1,62 @@
 const express = require('express');
-const cors = require("cors");
-const cookieSession = require("cookie-session");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
+const authRoutes = require('./routes/authRoutes');
+const dotenv = require('dotenv');
+
+const WebSocket = require('ws'); 
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(
-  cookieSession({name: "session", key: ["ankur"], maxAge:24*60*60*100})  
-);
 
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(cors({
-    origin:"https://localhost:5173",
-    methods:"GET,POST,PUT,DELETE",
-    credentails:true
-}));
-const port = 5000;
-app.listen(port, () => console.log('App listning to Port ' + port));
+// MongoDB connection
+uri = process.env.MONGODB_URL;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('MongoDB connected successfully');
+})
+.catch((error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(authRoutes);
+
+// Create a WebSocket server
+const wss = new WebSocket.Server({ noServer: true });
+
+// Handle WebSocket connections
+wss.on('connection', (ws) => {
+  console.log('WebSocket client connected');
+
+  // Handle incoming messages
+  ws.on('message', (message) => {
+    console.log('Received message:', message);
+    // Broadcast the message to all clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+// Upgrade HTTP server to handle WebSocket requests
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Attach WebSocket server to the HTTP server
+server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit('connection', ws, request);
+  });
+});
